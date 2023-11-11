14cd08d110c87663ac5605d3d713d120219a7070
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-11-11 13:01:29 -0600

    added my own comments for what needs to be changed based on review of the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+// Import ApolloClient, ApolloProvider, InMemoryCache from apollo/client//
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+
+//set up client = new ApolloClient, then wrap Navbar and Outlet in ApolloProvider where client={client}//
 
 function App() {
   return (
