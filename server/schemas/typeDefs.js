3ec327e30ee0faa057ff4eccfcb10783d7afc0be
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-11-12 13:48:28 -0600

    set up Apollo Server, created resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const typeDefs = `
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+}
+
+type Book {
+    _id: ID!
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Query {
+    user: [User]
+}
+`;
+
+module.exports = typeDefs;
